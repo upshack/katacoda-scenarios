02d1190beeb916654b96eba612238ad280869502
upshack <onuralemdar1998@gmail.com>
2021-07-08 15:02:33 +0300

    asdas

--- test-scenarioo/background.sh
+++ test-scenarioo/background.sh
@@ -7,5 +7,5 @@ mkdir dizin1
 mkdir dizin2
 mkdir dizin3
 
-touch /dizin/lorem.txt 
-echo "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum." >> /dizin/lorem.txt 
+touch /dizin1/lorem.txt 
+echo "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum." >> /dizin1/lorem.txt 
