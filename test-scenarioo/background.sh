96f710dc32046f9dc313a3217d51e5f21f7dd9bc
upshack <onuralemdar1998@gmail.com>
2021-07-08 14:52:00 +0300

    sdfds

--- test-scenarioo/background.sh
+++ test-scenarioo/background.sh
@@ -1,3 +1,5 @@
-useradd -p $(openssl passwd -crypt 123) onurr
-su onurr
-123
+#!/usr/bin/env bash
+useradd -p $(openssl passwd -crypt 123) deneme
+useradd -p $(openssl passwd -crypt 1234) deneme1
+useradd -p $(openssl passwd -crypt 12345) deneme2
+su --login onurr -s 123
