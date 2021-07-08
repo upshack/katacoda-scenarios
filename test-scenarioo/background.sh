9f5e5fbdacf2415c6ed701e7c2c4e4226f029ff0
upshack <onuralemdar1998@gmail.com>
2021-07-08 14:58:33 +0300

    asdas

--- test-scenarioo/background.sh
+++ test-scenarioo/background.sh
@@ -2,5 +2,5 @@
 useradd -p $(openssl passwd -crypt 123) deneme
 useradd -p $(openssl passwd -crypt 1234) deneme1
 useradd -p $(openssl passwd -crypt 12345) deneme2
-su --login onurr -s 123
-echo "onur"
+su deneme
+123
