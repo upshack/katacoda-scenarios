adb7846294b2616aa63add368144470dc19270ee
upshack <onuralemdar1998@gmail.com>
2021-07-08 15:23:54 +0300

    dfgdgfd

--- test-scenarioo/background.sh
+++ test-scenarioo/background.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 # User ekledik
-useradd -p $(openssl passwd -crypt 123) deneme
-useradd -p $(openssl passwd -crypt 1234) deneme1
-useradd -p $(openssl passwd -crypt 12345) deneme2
+useradd -p $(openssl passwd -crypt 123) kullanici
+useradd -p $(openssl passwd -crypt 1234) kullanici2
+useradd -p $(openssl passwd -crypt 12345) kullanici3
 
 # Dizin Olustruduk 
 mkdir dizin1
@@ -23,4 +23,3 @@ echo "kVjtWrEE){.HJq5w" >> .gizli.txt
 touch lorem.txt 
 echo "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum." >> lorem.txt 
 mv lorem.txt dizin1
-
